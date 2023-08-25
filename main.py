58351d042cdb7612efcb5b92aa062b6da6277c75
Yuriy Malkov <yura345@gmail.com>
2023-08-24 22:23:47 -0400

    renamed fields so they make more sense

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 import example1.generated.user_pb2 as user_pb2
 
 
-def get_existing_tables(cursor):
+def get_database_tables(cursor):
     cursor.execute("SHOW TABLES")
-    return [table[0] for table in cursor.fetchall()]
+    return [columns[0] for columns in cursor.fetchall()]
 
 
 def get_existing_primary_fields(cursor, db_name, table_name):
@@ -15,16 +15,21 @@ def get_existing_primary_fields(cursor, db_name, table_name):
                                 f"AND TABLE_NAME = '{table_name}' "
                                 f"AND COLUMN_KEY = 'PRI'")
     cursor.execute(get_primary_fields_query)
-    return [table[0] for table in cursor.fetchall()]
+    return [columns[0] for columns in cursor.fetchall()]
 
 
 def get_existing_fields(cursor, db_name, table_name):
-    get_existing_fields_query = (f"SELECT COLUMN_NAME "
+    result_list: list = []
+    get_existing_fields_query = (f"SELECT COLUMN_NAME, DATA_TYPE, COLUMN_DEFAULT, IS_NULLABLE, EXTRA, COLUMN_KEY "
                                  f"FROM INFORMATION_SCHEMA.COLUMNS "
                                  f"WHERE TABLE_SCHEMA = '{db_name}' "
                                  f"AND TABLE_NAME = '{table_name}';")
     cursor.execute(get_existing_fields_query)
-    return [table[0] for table in cursor.fetchall()]
+    for columns in cursor.fetchall():
+        filtered_columns = [item for item in columns[1:] if item is not None and item.strip() != '']
+        filtered_columns_map = {columns[0]: filtered_columns}
+        result_list.append(filtered_columns_map)
+    return result_list
 
 
 def create_table_if_not_exists(cursor, table_name, fields):
@@ -36,8 +41,8 @@ def create_table_if_not_exists(cursor, table_name, fields):
     cursor.execute(create_table_query)
 
 
-def get_primary_key_fields(message_descriptor):
-    return [field.name for field in message_descriptor.fields if field.GetOptions().Extensions[user_pb2.primaryKey]]
+def get_primary_key_fields(proto_message):
+    return [field.name for field in proto_message.fields if field.GetOptions().Extensions[user_pb2.primaryKey]]
 
 
 def create_primary_key_constraint(cursor, table_name, primary_key_fields):
@@ -53,13 +58,13 @@ def drop_table(cursor, table_name):
 
 
 # Determine the table name based on the Protocol Buffers class name
-def get_table_name(message_descriptor):
-    return message_descriptor.name.lower()
+def get_proto_table_name(proto_message):
+    return proto_message.name.lower()
 
 
 # Get the fields from the Protocol Buffers schema
-def get_fields(message_descriptor):
-    return message_descriptor.fields
+def get_proto_fields(proto_message):
+    return proto_message.fields
 
 
 #  Get fields names from the Protobuf Buffers schema
@@ -104,47 +109,50 @@ def add_new_fields(cursor, table_name, fields):
                                  f"{primary_key} "
                                  f"{auto_increment}, ")
     add_new_fields_query = add_new_fields_query.rstrip(", ")
-    print(add_new_fields_query)
     cursor.execute(add_new_fields_query)
 
 
-def synchronize_tables_with_proto(db_messages, connection):
+def synchronize_tables_with_proto(proto_messages, connection):
     db_name = connection.database
     cursor = connection.cursor()
-    existing_tables = get_existing_tables(cursor)
+    database_tables = get_database_tables(cursor)
 
-    for message_descriptor in db_messages:
-        table_name = get_table_name(message_descriptor)
-        fields = get_fields(message_descriptor)
-        primary_key_fields: list = get_primary_key_fields(message_descriptor)
+    for proto_message in proto_messages:
+        proto_table_name = get_proto_table_name(proto_message)
+        proto_fields = get_proto_fields(proto_message)
+        # TODO deprecate reliance on primary_key_fields
+        primary_key_fields: list = get_primary_key_fields(proto_message)
 
-        if table_name not in existing_tables:
-            create_table_if_not_exists(cursor, table_name, fields)
-            create_primary_key_constraint(cursor, table_name, primary_key_fields)
+        if proto_table_name not in database_tables:
+            # TODO refactor create_table_if_not_exists() so it can add primary keys as well
+            create_table_if_not_exists(cursor, proto_table_name, proto_fields)
+            # TODO deprecate create_primary_key_constraint
+            create_primary_key_constraint(cursor, proto_table_name, primary_key_fields)
             connection.commit()
-
         else:
-            fields_names: list = get_fields_name(fields)
-            existing_fields_names: list = get_existing_fields(cursor, db_name, table_name)
-            existing_primary_key_fields: list = get_existing_primary_fields(cursor, db_name, table_name)
+            fields_names: list = get_fields_name(proto_fields)
+            existing_fields_schema: list = get_existing_fields(cursor, db_name, proto_table_name)
+
+            existing_fields_names: list = [list(field.keys())[0] for field in existing_fields_schema]
+            existing_primary_key_fields: list = [list(field.keys())[0] for field in existing_fields_schema if 'PRI' in field.get(list(field.keys())[0], [])]
 
             # Check if fields were dropped and/or added
             if set(fields_names) != set(existing_fields_names):
                 fields_to_drop: set = set(existing_fields_names).difference(set(fields_names))
-                drop_existing_fields(cursor, table_name, fields_to_drop)
-                add_new_fields(cursor, table_name, fields)
+                drop_existing_fields(cursor, proto_table_name, fields_to_drop)
+                add_new_fields(cursor, proto_table_name, proto_fields)
                 connection.commit()
 
-            # Check if field attributes were changed
+            # Check if primary keys were updated
             if set(primary_key_fields) != set(existing_primary_key_fields):
                 updated_primary_keys = ", ".join(primary_key_fields)
-                update_primary_keys(cursor, table_name, updated_primary_keys)
+                update_primary_keys(cursor, proto_table_name, updated_primary_keys)
                 connection.commit()
 
     # Drop tables that are no longer in the schema
-    for table_name in existing_tables:
-        if table_name not in [get_table_name(msg) for msg in db_messages]:
-            drop_table(cursor, table_name)
+    for table_name in database_tables:
+        if proto_table_name not in [get_proto_table_name(proto_message) for proto_message in proto_messages]:
+            drop_table(cursor, proto_table_name)
             connection.commit()
 
     cursor.close()
@@ -165,9 +173,10 @@ def main():
         sys.exit(1)
 
     # Usage
-    messages = user_pb2.DESCRIPTOR.message_types_by_name.values()
-    db_messages = [message for message in messages if message.GetOptions().Extensions[user_pb2.dbTable]]
-    synchronize_tables_with_proto(db_messages, connection)
+    proto_schema = user_pb2.DESCRIPTOR
+    proto_messages: list = proto_schema.message_types_by_name.values()
+    proto_messages: list = [message for message in proto_messages if message.GetOptions().Extensions[user_pb2.dbTable]]
+    synchronize_tables_with_proto(proto_messages, connection)
     connection.close()
 
 
